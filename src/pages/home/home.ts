cae52d81cd2d7ba36f91f3885cc8f54074c39fca
juanlopes <hackt446@hotmail.com>
2018-09-07 16:02:45 -0400

    Pausa Aula 2 - Atividade 3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Carro } from '../../modelos/carro';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public carros: Carro[];
 
+  constructor(public navCtrl: NavController, private _http: HttpClient) {
+    this._http.get<Carro[]>('http://localhost:8080/api/carro/listaTodos')
+              .subscribe(
+                (carros) => {
+                  this.carros = carros;
+                }
+              );
   }
 
 }
