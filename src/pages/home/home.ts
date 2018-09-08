07b37a21c3d57c8aa9bcd7392c702a5bf7eb717e
juanlopes <hackt446@hotmail.com>
2018-09-08 14:10:17 -0400

    Aula 3 - Atividade 7: Fazendo a segunda tela do programa.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,54 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Carro } from '../../modelos/carro';
-import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CarrosServiceProvider } from '../../providers/carros-service/carros-service';
+import { NavLifecycles } from '../../utils/ionic/nav/nav-lifecycles';
+import { EscolhaPage } from '../escolha/escolha';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements NavLifecycles {
 
   public carros: Carro[];
 
-  constructor(public navCtrl: NavController, private _http: HttpClient) {
-    this._http.get<Carro[]>('http://localhost:8080/api/carro/listaTodos')
+  constructor(public navCtrl: NavController,
+              private _loadingCtrl: LoadingController,
+              private _alertCtrl: AlertController,
+              private _carrosService: CarrosServiceProvider) {}
+  ionViewDidLoad() {
+    let loading = this._loadingCtrl.create({
+      content: 'Carregando carros...'
+    });
+
+    loading.present();
+
+    this._carrosService.lista()
               .subscribe(
                 (carros) => {
                   this.carros = carros;
+                  loading.dismiss();
+                },
+                (err: HttpErrorResponse) => {
+                  loading.dismiss();
+
+                  this._alertCtrl.create({
+                    title: 'Falha na comunicação',
+                    subTitle: 'Não foi possível carregar a lista carros. Tente novamente mais tarde.',
+                    buttons: [
+                      {text: 'OK'}
+                    ]
+                  }).present();
                 }
               );
+  }
+
+  selecionaCarro(carro: Carro) {
+    this.navCtrl.push(EscolhaPage, {
+      carroSelecionado: carro
+    });
   }
 
 }
